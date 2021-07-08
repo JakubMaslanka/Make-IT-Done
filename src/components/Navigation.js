8e52aa897b4675f2ad19c8e2c46a270d5584f075
JakubMaslanka <kuba.ath51@gmail.com>
2021-07-08 20:27:42 +0200

    Change whole files structure in the project

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <div className="Nav">
+      <button type="button">Home</button>
+    </div>
+  );
+}
+
+export default Navigation;
